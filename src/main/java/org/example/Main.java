32d24dec5fe4caefa005143c3768c4a680d2ab31
Julia Mazur <julia21.kwiat@gmail.com>
2023-03-19 21:08:09 +0100

    dodanie podstawowych metod do kalkulatora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,27 @@ public class Main {
         System.out.println("Menu: \n 1.dodawanie \n 2.odejmowanie \n 3.dzielenie \n 4. mnożenie.");
         BufferedReader konsola = new BufferedReader(new InputStreamReader(System.in));
         int wybor = Integer.parseInt(konsola.readLine());
-
+             int liczba1 = Integer.parseInt(konsola.readLine());
+        int liczba2 = Integer.parseInt(konsola.readLine());
+        int wynik = 0;
+        switch(wybor) {
+            case 1:
+                wynik = dodawanie(liczba1, liczba2);
+                break;
+            case 2:
+                wynik = odejmowanie(liczba1, liczba2);
+                break;
+            case 3:
+                wynik = dzielenie(liczba1, liczba2);
+                break;
+            case 4:
+                wynik = mnożenie(liczba1, liczba2);
+                break;
+            default:
+                System.out.println("Nie ma takiej metody");
+                break;
+        }
+        System.out.println(wynik);
     }
 
 }
